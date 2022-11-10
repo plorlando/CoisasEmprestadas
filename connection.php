d2edd995798d5538c1a0dc699f7cbf75eb2a14ff
Pedro Lor Orlando <48284191+plorlando@users.noreply.github.com>
2022-11-09 22:40:13 -0300

    commit

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$_con = new mysqli("localhost", "root", "") or die("Connect failed: %s\n". $_con -> error);;
+$_con = new mysqli("localhost", "root", "") or die("Connect failed: %s\n". $_con -> error);
 ?>
