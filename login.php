81babdd274da43ca04fad0195c68c9c6c160afc4
Pedro Lor Orlando <48284191+plorlando@users.noreply.github.com>
2022-11-23 22:06:45 -0300

    commit 23-11-2022

--- login.php
+++ login.php
@@ -10,9 +10,16 @@ if(empty($_POST['cpUser']) || empty($_POST['cpSenha'])) {
 $user = $_POST['cpUser'];
 $pass = $_POST['cpSenha'];
 
-$sql_check_user = "SELECT userId, user FROM users WHERE user = '$user' and userPass = md5('$pass')";
+$sql_check_user = "SELECT userId, user, userName FROM users WHERE user = '$user' and userPass = md5('$pass')";
 $result = mysqli_query($conn, $sql_check_user);
 $row = mysqli_num_rows($result);
+
+$now = date("d/m/Y H:i:s");
+setcookie("user", $result['userName'], time()+60*60*24);
+setcookie("last_access", $now, time()+60*60*24);
+
+
+
 
 if($row == 1) {
     $_SESSION['user'] = $user;
